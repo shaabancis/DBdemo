139ca919458551c68f3fb59db36b9c053c5085ca
shaabancis <shaabancis@gmail.com>
2020-03-24 10:49:15 +0300

    -

--- dbo/Tables/Answer.sql
+++ dbo/Tables/Answer.sql
@@ -2,8 +2,10 @@
     [Id]          UNIQUEIDENTIFIER NOT NULL,
     [Description] NVARCHAR (100)   NOT NULL,
     [QuestionId]  UNIQUEIDENTIFIER NOT NULL,
-    [Name] NVARCHAR(50) NULL, 
+    [name] NVARCHAR(50) NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Answer_To_Question] FOREIGN KEY ([QuestionId]) REFERENCES [dbo].[Question] ([Id]) ON DELETE CASCADE
 );
+
+
 
---
+++ dbo/Tables/TestDemo.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[TestDemo]
+(
+	[name] INT NOT NULL 
+)
